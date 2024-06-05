b877998617acc7be2cd627d0c4d2bc06b42c0579
il42133 <82491273+il42133@users.noreply.github.com>
2024-06-05 16:17:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const SearchQuery = require('./models/SearchQuery');  // Import the SearchQuery model
 
 app.set('view engine', 'ejs');
@@ -54,5 +54,5 @@ app.post('/search', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running on port ${port}`);
 });
